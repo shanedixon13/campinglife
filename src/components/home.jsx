9623f6b268c113cf3d8750e6a8bc53f7bd48cd43
Shane Dixon <shanedixon13@gmail.com>
2021-10-27 22:04:50 -0500

    Updated CSS

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,11 +3,13 @@ import "./home.css";
 const Home = () => {
 	return (
 		<div className="home-page">
-			<h1>Welcome to CampingLife</h1>
-			<p>
-				Where you are just a click away from the tools you need for your
-				journey.
-			</p>
+			<div className="welcome-message">
+				<h1>Welcome to CampingLife</h1>
+				<p>
+					Where you are just a click away from the tools you need for
+					your journey.
+				</p>
+			</div>
 		</div>
 	);
 };
