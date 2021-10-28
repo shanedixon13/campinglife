39cd31aac3fdac341b8d2d212603be3cd43e970d
Shane Dixon <shanedixon13@gmail.com>
2021-10-27 23:10:17 -0500

    CSS Changes

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,14 +1,22 @@
 import "./home.css";
+import sunrise from "./css_backgrounds/sunrise.jpg";
+import night from "./css_backgrounds/night.jpg";
 
 const Home = () => {
 	return (
 		<div className="home-page">
+			<div className="home-small-photo">
+				<img src={sunrise} alt="Sunrise" />
+			</div>
 			<div className="welcome-message">
 				<h1>Welcome to CampingLife</h1>
 				<p>
 					Where you are just a click away from the tools you need for
 					your journey.
 				</p>
+			</div>
+			<div className="home-small-photo">
+				<img src={night} alt="Night on Lake" />
 			</div>
 		</div>
 	);
